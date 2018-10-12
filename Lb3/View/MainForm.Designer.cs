64844b3033a713f800fc902c1c9faaa4c09ed3b6
Alex <oleksandr.torba@nure.ua>
2018-10-12 13:06:17 +0300

    lb3 added logic to listBox

--- Lb3/View/MainForm.Designer.cs
+++ Lb3/View/MainForm.Designer.cs
@@ -36,7 +36,7 @@
             this.pcTasksListBox = new System.Windows.Forms.ListBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.addTaskButton = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -91,6 +91,7 @@
             this.pcComboBox.Name = "pcComboBox";
             this.pcComboBox.Size = new System.Drawing.Size(145, 28);
             this.pcComboBox.TabIndex = 2;
+            this.pcComboBox.SelectionChangeCommitted += new System.EventHandler(this.pcComboBox_SelectionChangeCommitted);
             // 
             // label1
             // 
@@ -110,6 +111,7 @@
             this.pcTasksListBox.Name = "pcTasksListBox";
             this.pcTasksListBox.Size = new System.Drawing.Size(298, 264);
             this.pcTasksListBox.TabIndex = 2;
+            this.pcTasksListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pcTasksListBox_MouseDoubleClick);
             // 
             // pictureBox1
             // 
@@ -124,7 +126,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.clearButton);
             this.groupBox2.Controls.Add(this.addTaskButton);
             this.groupBox2.Controls.Add(this.pictureBox2);
             this.groupBox2.Controls.Add(this.label4);
@@ -137,16 +139,16 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tasks";
             // 
-            // button1
+            // clearButton
             // 
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 9F);
-            this.button1.Location = new System.Drawing.Point(10, 92);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 29);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Clear";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.clearButton.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.clearButton.Location = new System.Drawing.Point(10, 92);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(145, 29);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // addTaskButton
             // 
@@ -210,7 +212,7 @@
             this.Controls.Add(this.transferButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
             this.Text = "ComputerManager";
             this.groupBox1.ResumeLayout(false);
@@ -238,7 +240,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ListBox taskListBox;
         private System.Windows.Forms.Button transferButton;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- Lb3/View/MainForm.cs
+++ Lb3/View/MainForm.cs
@@ -12,7 +12,6 @@ namespace Lb3
 {
     public partial class MainForm : Form
     {
-
         ComputerManager computerManager = new ComputerManager();
 
         public MainForm()
@@ -43,14 +42,43 @@ namespace Lb3
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void clearButton_Click(object sender, EventArgs e)
         {
             taskListBox.DataSource = null;
         }
 
+        private void RefreshLists(List<UsersTask> pcTasks, List<UsersTask> tasksList)
+        {
+            pcTasksListBox.DataSource = pcTasks.ToList();
+            taskListBox.DataSource = tasksList.ToList();
+        }
+
         private void transferButton_Click(object sender, EventArgs e)
         {
+            UsersTask usersTask = taskListBox.SelectedItem as UsersTask;
 
+            if (computerManager.TasksDictionary.Count != 0)
+                computerManager.TasksDictionary.Remove(usersTask.GetHashCode());
+
+            Computer computer = pcComboBox.SelectedItem as Computer;
+
+            computer.Tasks.Add(usersTask);
+
+            RefreshLists(computer.Tasks, computerManager.TasksDictionary.Values.ToList());
+
+        }
+
+        private void pcComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            ComboBox computers = sender as ComboBox;
+            Computer computer = computers.SelectedValue as Computer;
+
+            pcTasksListBox.DataSource = computer.Tasks?.ToList();
+        }
+
+        private void pcTasksListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("pcTasksListBox_MouseDoubleClick ");
         }
     }
 }
